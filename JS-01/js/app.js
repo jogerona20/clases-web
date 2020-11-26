7bec57a152c2229dec46b99bd90e023496901f73
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-11-26 00:10:58 -0700

    Update app.js

--- JS-01/js/app.js
+++ JS-01/js/app.js
@@ -1,55 +1,54 @@
-console.log("Hola Mundo");
+console.log("Hola de nuevo");
 
-//Variables: var y let
+// VARIABLES: var y let
 
 var mes = "Octubre";
 console.log(typeof(mes));
 
-var dia=21;
+var dia = 21;
 console.log(typeof(dia));
 
 var esFestivo;
-esFestivo=null;
+esFestivo = null;
 console.log(esFestivo);
 
 var var1, var2, var3;
 
-dia = 'veiniuno';
+dia = 'veintiuno';
 console.log(typeof(dia));
 
 console.log(document);
 
-function suma(a, b){
-	document.getElementByid('resultado').innerHTML = a + b;
-	alert("hiciste click");
+function suma(a, b) {
+    document.getElementById('resultado').innerHTML = a + b;
+    alert("En hiciste clic");
 }
 
 //LocalStorage
 
-var impirmir =function(){
-	document.getElementById("valor").innerHTML(dato);
+var imprimir = function (dato) {
+    document.getElementById("valor").innerHTML = dato;
 }
 
-var aumentar = function(){
-	LocalStorage.contador++;
-	impirmir(LocalStorage.contador);
+var aumentar = function() {
+    localStorage.contador++;
+    imprimir(localStorage.contador);
 }
 
-var reiniciar = function(){
-	LocalStorage.contador = 0;
-	impirmir(LocalStorage.contador);
+var reiniciar = function() {
+    localStorage.contador = 0;
+    imprimir(localStorage.contador);
 }
 
-var eliminar = function(){
-	LocalStorage.removeItem("contador");
-	impirmir(LocalStorage.contador);
+var eliminar = function() {
+    localStorage.removeItem("contador");
+    imprimir(localStorage.contador);
 }
 
-if(typeof(Storage) != "undefined"){
-	if(LocalStorage.contador){
-		impirmir(LocalStorage.contador);
-	}else{
-		LocalStorage.contador = 0;
-	}
+if (typeof(Storage) != "undefined") {
+    if(localStorage.contador){
+        imprimir(localStorage.contador);
+    } else {
+        localStorage.contador = 0;
+    }
 }
-
