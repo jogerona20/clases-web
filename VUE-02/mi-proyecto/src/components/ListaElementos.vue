a10cd0601a1b8aad1016e8c2673b90e171e05e92
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-11-27 18:48:38 -0700

    Create ListaElementos.vue

---
+++ VUE-02/mi-proyecto/src/components/ListaElementos.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="el in elementos" :key="el.id">
+                {{el.nombre}} - {{el.cantidad}}
+            </li>
+        </ul>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'ListaElementos',
+    props:{
+        elementos: Array,
+        titulo: {
+            type: String,
+            default: 'Mi lista'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
