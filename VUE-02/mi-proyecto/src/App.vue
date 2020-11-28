3bc28d600db57bcd090c5b850a2f3763e96afe11
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-11-27 18:48:40 -0700

    Update App.vue

--- VUE-02/mi-proyecto/src/App.vue
+++ VUE-02/mi-proyecto/src/App.vue
@@ -1,20 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld :msg="titulo" fecha="2020-10-28"/>
-
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="titulo" fecha="2020-10-28"/>
+    <ListaElementos titulo="Mi invetario de frutas" :elementos="frutas"/>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ListaElementos from './components/ListaElementos.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ListaElementos
   },
   data(){
    return {
-      titulo: "Mi Titutlo en vue"
+      titulo: "Mi Titutlo en vue",
+      frutas: [
+        {id: 1,nombre: 'Manzana', cantidad: 10},
+        {id: 2,nombre: 'Uva', cantidad: 5},
+        {id: 3,nombre: 'Fresa', cantidad: 7}
+      ]
     }
   }
 }
