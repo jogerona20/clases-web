281d84f08bdc96ad96929db555199fcd02e298ab
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-12-02 00:12:26 -0700

    Update Home.vue

--- VUE-04/proyecto-vuex/src/views/Home.vue
+++ VUE-04/proyecto-vuex/src/views/Home.vue
@@ -1,20 +1,36 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" width="30">
-    <p>Mi total:{{ total}}</p>
+    <h1>{{titulo}}</h1>
+    <p :style="colorTotal">Mi total:{{ total}}</p>
+    <button @click="accionAccion(5)">Aumentar</button>
     <p>{{ $store.state.otra}}</p>
+    <BotonDisminuir />
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex';
+import {mapState,mapActions} from 'vuex';
+import BotonDisminuir from '../components/BotonDisminuir'
 
 export default {
   name: 'Home',
   components: {
+     BotonDisminuir
+  },
+  data() {
+    return{
+      titulo: 'Mi Poyecto con vuex'
+    }
+  },
+  methods:{
+    ...mapActions(['accionAccion'])
   },
   computed:{
-    ...mapState(['total'])
+    ...mapState(['total']),
+    colorTotal(){
+      return this.total >= 100 ? {'color':'green'} : {'color' : 'red'};
+    }
   }
 }
 </script>
