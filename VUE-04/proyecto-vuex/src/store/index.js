013e0e167c0e7b938a91947fc8096aa95ef19f8e
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-12-02 00:12:23 -0700

    Update index.js

--- VUE-04/proyecto-vuex/src/store/index.js
+++ VUE-04/proyecto-vuex/src/store/index.js
@@ -5,12 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    total:100,
+    total:90,
     otra:'hola'
   },
   mutations: {
+    aumentar(state,num){
+      state.total = state.total + num;
+    },
+    disminuir(state){
+      state.total--;
+    }
   },
   actions: {
+    accionAccion({commit},num){
+      commit('aumentar',num)
+    },
+    accionDisminuir({commit}){
+      commit('disminuir');
+    }
   },
   modules: {
   }
