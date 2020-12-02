02cf8de1531b06c7ff7a971ebf1617235cf51fb0
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-12-02 04:26:29 -0700

    Create About.vue

---
+++ VUE-05/vuex-axios/src/views/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Mis todos</h1>
+    <div v-for="todo in doneTodos" :key="todo.id">
+      <h3>{{todo.title}}</h3>
+      <p>{{todo.completed ? 'Completo' : 'Incompleto'}}</p>
+  <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState, mapGetters } from 'vuex'
+export default {
+  name: 'About',
+  methods: {
+    ...mapActions(['getTodos'])
+  },
+  created() {
+    this.getTodos()
+  },
+  computed: {
+    ...mapState(['todos']),
+    ...mapGetters(['doneTodos'])
+  }
+}
+</script>
+
+<style>
+</style>
