7de8c7bbadb64c1755e3b6201fac0b8fca1c1a50
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-12-02 00:12:21 -0700

    Create BotonDisminuir.vue

---
+++ VUE-04/proyecto-vuex/src/components/BotonDisminuir.vue
@@ -0,0 +1,16 @@
+<template>
+    <button @click="accionDisminuir()">Disminuir</button>
+</template>
+
+<script>
+import {mapActions} from 'vuex';
+
+export default {
+    name: 'BotonDisminuir',
+    methods:{
+        ...mapActions(['accionDisminuir'])
+    }
+}
+</script>
+
+</style>
