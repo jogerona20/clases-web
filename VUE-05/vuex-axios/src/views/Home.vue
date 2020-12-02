585b4fc995389668df93ec8a6a1f19947ac32784
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-12-02 04:26:32 -0700

    Create Home.vue

---
+++ VUE-05/vuex-axios/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>Mis posts</h1>
+    <div v-for="post in posts" :key="post.id">
+      <p>{{post.title}}</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  name: 'Home',
+  components: {
+  },
+  methods: {
+    ...mapActions(['getPosts'])
+  },
+  mounted() {
+    this.getPosts()
+  },
+  computed: {
+    ...mapState(['posts'])
+  }
+}
+</script>
