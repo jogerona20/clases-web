54c6f2b54cb44d0d3fdeaa4a3fc13153fd0dbe2c
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-12-01 19:22:43 -0700

    Create Home.vue

---
+++ VUE-04/proyecto-vuex/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" width="30">
+    <p>Mi total:{{ total}}</p>
+    <p>{{ $store.state.otra}}</p>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  computed:{
+    ...mapState(['total'])
+  }
+}
+</script>
