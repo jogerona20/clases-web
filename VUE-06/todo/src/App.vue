1e16d81a6a2bdcaa919ed82ef5f777c54df23f4d
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-12-02 23:18:18 -0700

    Create App.vue

---
+++ VUE-06/todo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div id="nav" class="navbar navbar-dark bg-dark">
+      <router-link to="/">Inicio</router-link> 
+    </div>
+    <router-view class="container mt-4"/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #ffffff;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
