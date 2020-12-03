658ee7bf41f188ab448c24dd8d869022b13b8afd
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-12-02 23:18:31 -0700

    Create Home.vue

---
+++ VUE-06/todo/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <h1>Mis ToDos</h1>
+      <router-link to="/agregar-todo" class="btn btn-success float-right mb-3">Agregar</router-link>
+        <TablaTodo />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import TablaTodo from '../components/TablaTodo';
+export default {
+    name: 'Home',
+    components: {
+        TablaTodo
+    },
+}
+</script>
+
+<style>
+</style>
