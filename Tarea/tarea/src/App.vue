b6cf3fc9bb763da975843e0d38e4c61ca1ae4cd2
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-12-04 21:27:43 -0700

    Create App.vue

---
+++ Tarea/tarea/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div id="nav">
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: black;
+}
+
+#nav a.router-link-exact-active {
+  color: black;
+}
+</style>
