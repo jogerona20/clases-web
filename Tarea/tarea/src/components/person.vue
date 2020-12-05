06c5fdca33545752cf30334564c05f4c7146b904
jogerona20 <70170972+jogerona20@users.noreply.github.com>
2020-12-04 21:27:52 -0700

    Create person.vue

---
+++ Tarea/tarea/src/components/person.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="datosConjunto">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'person',
+    props :{
+        
+    },
+    
+}
+</script>
+
+<style scoped>
+</style>
